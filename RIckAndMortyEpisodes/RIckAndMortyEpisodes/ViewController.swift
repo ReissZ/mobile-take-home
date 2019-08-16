6c3c446190e1312e3d79e07dee210371bf1079a0
ReissZ <reisszurbyk@live.ca>
2019-08-15 23:12:48 -0400

    forked repo, project creation and initial push

---
+++ RIckAndMortyEpisodes/RIckAndMortyEpisodes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RIckAndMortyEpisodes
+//
+//  Created by Reiss Zurbyk on 2019-08-15.
+//  Copyright © 2019 Reiss Zurbyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
